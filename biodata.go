355f3bc572230082e96a9c64e969499b41bbae03
ffznrr <ffznrr@gmail.com>
2024-03-03 16:17:24 +0700

    assignment1

---
+++ biodata.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type bio struct {
+	nama, alamat, pekerjaan, alasan string
+}
+
+func main() {
+
+	// var nama = [2]string{"fauzan", "maman"}
+	// fmt.Println(nama)
+	args := os.Args
+	var isi = [...]string{args[0], args[1]}
+	var ket = isi[1]
+	n, _ := strconv.Atoi(ket)
+
+	var student = [...]bio{
+		{nama: "fauzan", alamat: "Jakarta", pekerjaan: "belum ada", alasan: "karena coding itu keren"},
+		{nama: "maman", alamat: "Bandung", pekerjaan: "Admin", alasan: "karena coding itu mantapp"},
+		{nama: "Ridwan", alamat: "Tangerang", pekerjaan: "mahasiswa", alasan: "toppp"},
+	}
+
+	var hitung = n - 1
+
+	fmt.Println(student[hitung])
+
+}
