c50c0b01c97e75d063d2daa749646c5bfdf118dc
ffznrr <ffznrr@gmail.com>
2024-03-18 05:55:03 +0700

    assignment 1

--- biodata.go
+++ biodata.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-type bio struct {
+type Bio struct {
 	nama, alamat, pekerjaan, alasan string
 }
 
@@ -15,11 +15,12 @@ func main() {
 	// var nama = [2]string{"fauzan", "maman"}
 	// fmt.Println(nama)
 	args := os.Args
+
 	var isi = [...]string{args[0], args[1]}
 	var ket = isi[1]
 	n, _ := strconv.Atoi(ket)
 
-	var student = [...]bio{
+	var student = [...]Bio{
 		{nama: "fauzan", alamat: "Jakarta", pekerjaan: "belum ada", alasan: "karena coding itu keren"},
 		{nama: "maman", alamat: "Bandung", pekerjaan: "Admin", alasan: "karena coding itu mantapp"},
 		{nama: "Ridwan", alamat: "Tangerang", pekerjaan: "mahasiswa", alasan: "toppp"},
